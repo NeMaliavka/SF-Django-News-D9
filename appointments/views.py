0738a1c435768cd5c40212ed556ce274de7d0c84
Алиса <128479231+NeMaliavka@users.noreply.github.com>
2024-12-11 19:28:17 +0300

    Update views.py

--- appointments/views.py
+++ appointments/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, reverse, redirect
 from django.views import View
-from django.core.mail import EmailMultiAlternatives  # импортируем класс для создание объекта письма с html
+from django.core.mail import EmailMultiAlternatives, send_mail  # импортируем класс для создание объекта письма с html
 from datetime import datetime
-
+from django.utils import timezone
+from .models import Subscription, Category
 
 from django.template.loader import render_to_string  # импортируем функцию, которая срендерит наш html в текст
 from .models import Appointment
+from news.models import Post
 
 
 class AppointmentView(View):
@@ -39,3 +41,28 @@ class AppointmentView(View):
         msg.send()  # отсылаем
 
         return redirect('appointments:make_appointment')
+
+
+def notify_subscribers(article):
+    subscriptions = Subscription.objects.filter(category=article.category)
+    for subscription in subscriptions:
+        send_mail(
+            subject=f'New article in {article.category.name}',
+            message=f'New article: {article.title}\nRead more: {article.get_absolute_url()}',
+            from_email='alice-for-me@yandex.ru',
+            recipient_list=[subscription.user.email],
+        )
+
+def weekly_summary():
+    one_week_ago = timezone.now() - timezone.timedelta(days=7)
+    articles = Post.objects.filter(created_at__gte=one_week_ago)
+    subscriptions = Subscription.objects.all()
+    for subscription in subscriptions:
+        user_articles = articles.filter(category=subscription.category)
+        if user_articles.exists():
+            send_mail(
+                subject='Weekly article summary',
+                message='\n'.join([f'{article.title}: {article.get_absolute_url()}' for article in user_articles]),
+                from_email='alice-for-me@yandex.ru',
+                recipient_list=[subscription.user.email],
+            )
