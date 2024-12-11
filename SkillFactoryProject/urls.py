5f27b575241574355a7295d36487a503c5946265
Alisa comp <alisa.krushinina@mail.ru>
2024-12-11 19:08:40 +0300

    initial

---
+++ SkillFactoryProject/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('protect.urls')),
+    path('sign/', include('sign.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('posts/', include('news.urls')),
+    path('appointments/', include('appointments.urls')),
+]
