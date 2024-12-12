f68013c2931590fb6f93d11a41489c1b0572e6a8
Алиса <128479231+NeMaliavka@users.noreply.github.com>
2024-12-12 14:33:50 +0300

    Update apps.py

--- news/apps.py
+++ news/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+from django_apscheduler.jobstores import DjangoJobStore
+from apscheduler.schedulers.background import BackgroundScheduler
+from .models import send_weekly_newsletters  # Импортируйте Вашу функцию
 
 class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news'
+
+    def ready(self):
+        # Инициализация планировщика
+        scheduler = BackgroundScheduler()
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+        scheduler.add_job(send_weekly_newsletters, 'interval', weeks=1)
+        scheduler.start()
